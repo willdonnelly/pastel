27ddc39be2c3be16967b555c36f48d93aa25dfc6
Will Donnelly <will.donnelly@gmail.com>
2009-06-06 15:04:17 -0500

    Fixed XPM runner.

--- Graphics/Pastel/Runners/XPM.hs
+++ Graphics/Pastel/Runners/XPM.hs
@@ -3,4 +3,4 @@ module Graphics.Pastel.Runners.XPM ( runXPM ) where
 import Graphics.Pastel
 import Graphics.Pastel.Draw.XPM
 
-runXPM = putStr . drawPixmap
+runXPM (w,h) d = putStr $ drawPixmap (w,h) d
