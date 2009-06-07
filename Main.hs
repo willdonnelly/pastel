c2fe32927e56a81fcff18708d29c7e743ff410f2
Will Donnelly <will.donnelly@gmail.com>
2009-06-06 21:04:03 -0500

    Reverted main to GTK runner.

--- Main.hs
+++ Main.hs
@@ -7,4 +7,5 @@ import Graphics.Pastel.GTK
 import Graphics.Pastel.WX
 import Graphics.Pastel.XPM
 
-main = testWX (1280,800) $ zoom 4 $ circle purple $ zoom 0.5 $ canvas white
+main = testGTK (1280,800) drawing
+  where drawing = zoom 4 . circle purple . zoom 0.5 $ canvas white
